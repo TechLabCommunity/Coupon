d042b5ec738f51cb3694ff332f4027dc3f960c36
Andrea Tazio Giusti <andreatgiusti@gmail.com>
2019-08-03 16:55:22 +0200

    added coupon

---
+++ website/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Coupon(models.Model):
+
+    code = models.TextField()
+    already_used = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    used_by_seller = models.ForeignKey(User, unique=False, on_delete=models.CASCADE, blank=True, null=True)
